5f20aed8081784ae25cd79aa107b30a39cccdc67
nuhman-bititude <nuhman@bititude.com>
2022-09-22 14:48:11 +0530

    Workflow Completed

--- src/app/Components/employees/UI/employee-navs/Components/all-checks/vaccination-check/UI/submit-check/submit-check.component.ts
+++ src/app/Components/employees/UI/employee-navs/Components/all-checks/vaccination-check/UI/submit-check/submit-check.component.ts
@@ -1,5 +1,6 @@
 import { PartlyVaccinatedService } from './../add-evidence/manual-check/partly-vaccinated.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-submit-check',
@@ -7,26 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./submit-check.component.css'],
 })
 export class SubmitCheckComponent implements OnInit {
-  timer: boolean = false;
+  timer: boolean = true;
   firstDoseDate: string;
   secondDoseDate: string;
-  constructor(private VaccineService: PartlyVaccinatedService) {}
+  constructor(
+    private VaccineService: PartlyVaccinatedService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    // setTimeout(() => {
-    //   this.timer = false;
-    // }, 3000);
-    // this.firstDoseDate =
-    //   this.VaccineService.firstDoseDate.toString() +
-    //   ' ' +
-    //   this.VaccineService.firstDoseMonth +
-    //   ' ' +
-    //   this.VaccineService.firstDoseYear.toString();
-    // this.secondDoseDate =
-    //   this.VaccineService.SecondDoseDate.toString() +
-    //   ' ' +
-    //   this.VaccineService.SecondDoseMonth +
-    //   ' ' +
-    //   this.VaccineService.SecondDoseYear.toString();
+    setTimeout(() => {
+      this.timer = false;
+    }, 3000);
+
+    try {
+      this.firstDoseDate =
+        this.VaccineService.firstDoseDate.toString() +
+        ' ' +
+        this.VaccineService.firstDoseMonth +
+        ' ' +
+        this.VaccineService.firstDoseYear.toString();
+      this.secondDoseDate =
+        this.VaccineService.SecondDoseDate.toString() +
+        ' ' +
+        this.VaccineService.SecondDoseMonth +
+        ' ' +
+        this.VaccineService.SecondDoseYear.toString();
+    } catch (error) {
+      this.router.navigateByUrl('vaccination/add-partly-vaccinated');
+    }
   }
 }
